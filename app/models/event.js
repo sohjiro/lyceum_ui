858098c27d091c50cf08b62d5219b767f053339a
sohjiro <felipe@makingdevs.com>
2017-01-15 16:53:35 -0600

    adding model attribute for event

--- app/models/event.js
+++ app/models/event.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  type: DS.attr('string'),
+  name: DS.attr('string'),
+  starting_date: DS.attr('date'),
+  campus: DS.attr('string'),
+  quorum: DS.attr('number'),
+  price: DS.attr('number')
 });
