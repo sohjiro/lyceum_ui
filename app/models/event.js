d90abf2cb9af538c09621ef1c7c9627fcd91557a
sohjiro <felipe@makingdevs.com>
2017-01-15 21:06:58 -0600

    handling new event creation... WIP

--- app/models/event.js
+++ app/models/event.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   type: DS.attr('string'),
   name: DS.attr('string'),
-  starting_date: DS.attr('date'),
+  starting_date: DS.attr('string'),
   campus: DS.attr('string'),
   quorum: DS.attr('number'),
   price: DS.attr('number')
--- app/router.js
+++ app/router.js
@@ -7,7 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('event');
+  this.route('event', function() {
+    this.route('new');
+  });
 });
 
 export default Router;
