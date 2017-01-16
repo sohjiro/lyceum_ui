30a0e79dfe27c2a623addbf35c380ff7709cb9d4
sohjiro <felipe@makingdevs.com>
2017-01-16 00:26:04 -0600

    handling errors... WIP

--- app/event/new/route.js
+++ app/event/new/route.js
@@ -13,16 +13,9 @@ export default Ember.Route.extend({
       });
 
       new_event.save().then(() => {
-        console.log('save successful');
-        this.controller.set('message', 'A new event has been created');
-      }, function() {
-        this.controller.set('type', null);
-        this.controller.set('name', null);
-        this.controller.set('starting_date', null);
-        this.controller.set('campus', null);
-        this.controller.set('quorum', null);
-        this.controller.set('price', null);
-        this.controller.set('message', 'Saved failure');
+        this.controller.set('success', 'A new event has been created');
+      }, function(e) {
+        console.log(e);
       });
     }
   }
