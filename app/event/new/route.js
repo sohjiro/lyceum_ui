a7b9ea7ca6b1c8258016ff81a668b51c0d9654b1
sohjiro <felipe@makingdevs.com>
2017-01-17 23:51:23 -0600

    adding datepicker and showing form

--- app/event/new/route.js
+++ app/event/new/route.js
@@ -3,10 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     addNew: function() {
+      let starting_date = new Date(this.controller.get('starting_date'));
+
       var new_event = this.store.createRecord('event', {
         type: this.controller.get('type'),
         name: this.controller.get('name'),
-        starting_date: this.controller.get('starting_date'),
+        starting_date: starting_date.toJSON(),
         campus: this.controller.get('campus'),
         quorum: this.controller.get('quorum'),
         price: this.controller.get('price')
@@ -14,6 +16,12 @@ export default Ember.Route.extend({
 
       new_event.save().then(() => {
         this.controller.set('success', {message: 'A new event has been created'});
+        this.controller.set('type', null);
+        this.controller.set('name', null);
+        this.controller.set('starting_date', null);
+        this.controller.set('campus', null);
+        this.controller.set('quorum', null);
+        this.controller.set('price', null);
       }).catch((reason) => {
         this.controller.set('error', reason.errors);
         new_event.rollbackAttributes();
