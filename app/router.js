18a5be3821ec68eb4fe4436b750a6da2e26e77a2
sohjiro <felipe@makingdevs.com>
2017-01-20 17:51:26 -0600

    renaming route

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('event', function() {
+  this.route('events', function() {
     this.route('new');
     this.route('candidates', {path: ":id/candidates"});
   });
