5ac623f0685047ba931463309452e0b495435661
sohjiro <felipe@makingdevs.com>
2017-01-20 17:50:30 -0600

    adding candidates route

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('event', function() {
     this.route('new');
+    this.route('candidates', {path: ":id/candidates"});
   });
 });
 
