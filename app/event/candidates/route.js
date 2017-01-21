c50a74b8a34f2f9c5106c66fd54820bef0d26315
sohjiro <felipe@makingdevs.com>
2017-01-21 12:52:11 -0600

    adding messages and changing names for actions

--- app/event/candidates/route.js
+++ app/event/candidates/route.js
@@ -1,4 +1,33 @@
 import Ember from 'ember';
 
+function clear(controller) {
+  controller.set('name', null);
+  controller.set('degree', null);
+  controller.set('email', null);
+  controller.set('telephone', null);
+  controller.set('observations', null);
+}
+
 export default Ember.Route.extend({
+  actions: {
+    save: function() {
+      var candidate = this.store.createRecord('candidate', {
+        name: this.controller.get('name'),
+        degree: this.controller.get('degree'),
+        email: this.controller.get('email'),
+        telephone: this.controller.get('telephone'),
+        observations: this.controller.get('observations'),
+        event: this.controller.model
+      });
+
+      candidate.save().then(() => {
+        this.controller.set('success', {message: 'A new event has been created'});
+        clear(this.controller);
+      }).catch((reason) => {
+        this.controller.set('error', reason.errors);
+        this.controller.set('success', null);
+        candidate.rollbackAttributes();
+      });
+    }
+  }
 });
