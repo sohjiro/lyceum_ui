d5f666e646ecf0078336f403e98b1396d957cf67
sohjiro <felipe@makingdevs.com>
2017-02-04 01:40:19 -0600

    adding actions for adding a new subject

--- app/subjects/new/route.js
+++ app/subjects/new/route.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    cancel: function() {
+      this.controller.set('subject', null);
+      this.transitionTo('subjects');
+    },
+    add: function() {
+      var new_subject = this.store.createRecord('subject', {
+        name: this.controller.get('subject')
+      });
+
+      new_subject.save().then(() => {
+        this.controller.set('success', {message: 'A new event has been created'});
+        this.controller.set('error', null);
+      }).catch((reason) => {
+        this.controller.set('error', reason.errors);
+        this.controller.set('success', null);
+        new_subject.rollbackAttributes();
+      });
+    }
+  }
 });
