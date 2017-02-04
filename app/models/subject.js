aa38b803fd8749029fc3dce405fdef7368796887
sohjiro <felipe@makingdevs.com>
2017-02-03 23:53:03 -0600

    showing list of subjects elements

--- app/models/subject.js
+++ app/models/subject.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr('string')
+  name: DS.attr('string'),
+  inserted_at: DS.attr('string'),
+  updated_at: DS.attr('string')
 });
