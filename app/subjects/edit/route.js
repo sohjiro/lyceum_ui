dd0ebd56deb6976c7e82775b1d71d7a796cd34e4
sohjiro <felipe@makingdevs.com>
2017-02-04 02:13:44 -0600

    updating and canceling action for subjects

--- app/subjects/edit/route.js
+++ app/subjects/edit/route.js
@@ -3,5 +3,22 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.get('store').peekRecord('subject', params.subject_id);
+  },
+  actions: {
+    cancel: function() {
+      this.controller.model.reload();
+      this.transitionTo('subjects');
+    },
+    update: function() {
+      this.controller.model.save().then(() => {
+        this.controller.set('success', {message: 'Subject Updated'});
+        this.controller.set('error', null);
+        this.controller.set('name', null);
+      }).catch((reason) => {
+        this.controller.set('error', reason.errors);
+        this.controller.set('success', null);
+        this.controller.model.rollbackAttributes();
+      });
+    }
   }
 });
