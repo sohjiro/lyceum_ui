34e9066a1b4cfbc83cf8709b88f019b886574427
sohjiro <felipe@makingdevs.com>
2017-02-06 21:49:18 -0600

    adding a new candidate route

--- app/candidates/new/route.js
+++ app/candidates/new/route.js
@@ -1,4 +1,36 @@
 import Ember from 'ember';
 
+function clear(controller) {
+  controller.set('error', null);
+  controller.set('name', null);
+  controller.set('degree', null);
+  controller.set('email', null);
+  controller.set('telephone', null);
+}
+
 export default Ember.Route.extend({
+  actions: {
+    cancel: function() {
+      clear(this.controller);
+      this.transitionTo('candidates');
+    },
+    save: function() {
+      var new_candidate = this.createRecord('candidate', {
+        name: this.controller.get('name'),
+        degree: this.controller.get('degree'),
+        email: this.controller.get('email'),
+        telephone: this.controller.get('telephone')
+      });
+
+      new_candidate.save().then(() => {
+        this.controller.set('success', {message: 'Candidate created'});
+        this.controller.set('error', null);
+        clear(this.controller);
+      }).catch((reason) => {
+        this.controller.set('error', reason.errors);
+        this.controller.set('success', null);
+        new_candidate.rollbackAttributes();
+      });
+    }
+  }
 });
--- app/models/candidate.js
+++ app/models/candidate.js
@@ -4,6 +4,5 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   degree: DS.attr('string'),
   email: DS.attr('string'),
-  telephone: DS.attr('string'),
-  observations: DS.attr('string')
+  telephone: DS.attr('string')
 });
