d36dcb94c3afa2ffb10f3e6c6d1658d10dfec3e9
sohjiro <felipe@makingdevs.com>
2017-02-06 22:45:57 -0600

    changing add to events

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,5 +4,5 @@ export default Ember.Route.extend({
   setupController: function(controller, model) {
     controller.set('content', model);
     controller.set('campuses', this.get('store').findAll('campus'));
-  },
+  }
 });
