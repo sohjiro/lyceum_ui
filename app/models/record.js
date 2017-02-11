4efa60a2c610f1686ec813119c75e650db7aaebe
sohjiro <felipe@makingdevs.com>
2017-02-10 20:39:44 -0600

    adding computed property and showing current status

--- app/models/record.js
+++ app/models/record.js
@@ -1,8 +1,11 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   event: DS.belongsTo('event'),
   candidate: DS.belongsTo('candidate'),
+  observations: DS.attr('string'),
   record_status: DS.hasMany('record-status'),
-  observations: DS.attr('string')
+  recordSorting: ['inserted_at:desc'],
+  recordStatusSorted: Ember.computed.sort('record_status', 'recordSorting')
 });
