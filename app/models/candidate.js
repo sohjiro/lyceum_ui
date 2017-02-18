f4ce085a716c8b705ad8859b4aac4c070d0908d6
sohjiro <felipe@makingdevs.com>
2017-02-18 10:48:26 -0600

    adding relations

--- app/models/candidate.js
+++ app/models/candidate.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   degree: DS.attr('string'),
   email: DS.attr('string'),
-  telephone: DS.attr('string')
+  telephone: DS.attr('string'),
+  records: DS.hasMany('record')
 });
