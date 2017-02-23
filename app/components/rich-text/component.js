65287c73643c56be6fe625ade725792ee1cb3296
sohjiro <felipe@makingdevs.com>
2017-02-23 11:04:08 -0600

    updating value for component

--- app/components/rich-text/component.js
+++ app/components/rich-text/component.js
@@ -1,9 +1,15 @@
+// import $ from 'jquery';
 import Ember from 'ember';
 
 export default Ember.Component.extend({
   didInsertElement() {
-    this.$().summernote({
-      height: 300
-    });
+    this._super(...arguments);
+
+    let changeElement = (e, contents) => {
+      this.set('value', contents);
+    };
+
+    let summernote = this.$().summernote({ height: 300 });
+    summernote.on('summernote.change', (e, contents) => { changeElement(e, contents); });
   }
 });
