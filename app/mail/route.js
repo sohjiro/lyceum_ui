380a19de0582f5603195a9492febe95a650089b2
sohjiro <felipe@makingdevs.com>
2017-02-23 11:04:40 -0600

    showing data for event mail

--- app/mail/route.js
+++ app/mail/route.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  queryParams: { event_id: 0 },
+  model(params) {
+    return this.store.query('candidate', { event_id: params["event_id"] });
+  },
+
+  actions: {
+    send: function() {
+      var candidates = this.controller.get('candidates');
+      let subject = this.controller.get('subject');
+      let body = this.controller.get('data');
+
+      let data = `Mail send to ${candidates} with subject ${subject} and body ${body}`;
+
+      console.log(data);
+    }
+  }
 });
