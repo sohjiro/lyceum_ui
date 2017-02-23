5dad698ff4aabbee3e2f6a3520df86846be24ff8
sohjiro <felipe@makingdevs.com>
2017-02-23 15:50:40 -0600

    transforming candidates

--- app/event/mail/route.js
+++ app/event/mail/route.js
@@ -3,13 +3,22 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     send: function() {
-      var candidates = this.controller.get('candidates');
+      let records = this.controller.get('records') || [];
       let subject = this.controller.get('subject');
       let body = this.controller.get('data');
+      let to = records.mapBy('candidate.email').join(',');
 
-      let data = `Mail send to ${candidates} with subject ${subject} and body ${body}`;
+      let mail = this.store.createRecord('mail', {
+        to:  to,
+        subject: subject,
+        body: body
+      });
 
-      console.log(data);
+      mail.save().then(() => {
+        console.log("successfully send it");
+      }).catch((reason) => {
+        console.log(reason);
+      });
     }
   }
 });
